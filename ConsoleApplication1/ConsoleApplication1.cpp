68fd0a1efaa776dca526370b6aa2ab9f3f669510
shadyFCI <shady.20375787@compit.aun.edu.eg>
2021-11-01 23:56:55 +0200

    Added the files

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello World!\n";
+	
+}
